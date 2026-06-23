ad9384882ea473fc8ffc139473ef5447c41e338c
mvr <mitchell.v.riley@gmail.com>
2026-06-23 21:31:53 +0100

    Checkpoint

--- grammar.js
+++ grammar.js
@@ -1,4 +1,4 @@
-function command(rule, arg) {
+function command_seq(rule, arg) {
   return seq("\\", rule, arg);
 }
 
@@ -32,17 +32,9 @@ module.exports = grammar({
         $.def,
         $.fun,
         $.alloc,
-        $.tex_package,
-        $.taxon,
-        $.meta,
-        $.import,
-        $.export,
         $.namespace,
-        $.transclude,
         $.let,
         $.datalog,
-        $.diagram,
-        $.tikzfig,
         $.tex,
         $.scope,
         $.subtree,
@@ -50,19 +42,12 @@ module.exports = grammar({
         $.default,
         $.get,
         $.open,
-        $.execute,
-        $.route_asset,
-        $.current_tree,
-        $.syndicate_query_as_json_blob,
-        $.syndicate_current_tree_as_atom_feed,
         $.xml_tag,
         $.decl_xmlns,
-        $.query_tree,
         $.object,
         $.patch,
         $.call,
         $.hash_ident,
-        $._prim,
         $.verbatim_block,
         $.linebreak,
         $.control_symbol,
@@ -74,117 +59,61 @@ module.exports = grammar({
         $.braces,
         $.text,
         $.comment,
-        $.title,
-        $.author,
-        $.author_literal,
-        $.contributor,
-        $.contributor_literal,
-        $.date,
-        $.parent,
-        $.number,
-        $.tag,
-        $.ref,
-        $.ident,
+        $.command,
       ),
 
     braces: ($) => braces(optional(repeat1($._node))),
     squares: ($) => squares(optional(repeat1($._node))),
     parens: ($) => parens(optional(repeat1($._node))),
 
-    title: ($) => field("title", command("title", $._arg)),
-    author: ($) => field("author", command("author", $._arg)),
-    author_literal: ($) =>
-      field("author_literal", command("author/literal", $._arg)),
-    contributor: ($) =>
-      field("contributor", command("contributor", $._arg)),
-    contributor_literal: ($) =>
-      field("contributor_literal", command("contributor/literal", $._arg)),
-    date: ($) => field("date", command("date", $._txt_arg)),
-    parent: ($) => field("parent", command("parent", $._arg)),
-    number: ($) => field("number", command("number", $._txt_arg)),
-    tag: ($) => field("tag", command("tag", $._arg)),
-    ref: ($) =>
-      field(
-        "ref",
-        choice(
-          prec(1, command("ref", braces(field("target", $.addr)))),
-          command("ref", $._arg),
-        ),
-      ),
-
     xml_tag: ($) => seq("\\<", $._xml_qname, ">"),
     decl_xmlns: ($) => seq("\\xmlns:", $._xml_base_ident, $._txt_arg),
 
-    year: ($) => /[0-9]{4}/,
-    month: ($) => /(1[012]|0?[1-9])/,
-    day: ($) => seq(/[0123]/, /[0-9]/),
-    def: ($) => command("def", $.fun_spec),
+    def: ($) => command_seq("def", $.fun_spec),
     fun: ($) =>
-      command(
+      command_seq(
         "fun",
         seq(
           field("binder", repeat(squares($.text))),
-          field("argument", $._arg),
+          field("argument", $.argument),
         ),
       ),
-    tex_package: ($) => command("tex_package", $._txt_arg),
-    alloc: ($) => command("alloc", $.ident),
-    taxon: ($) => command("taxon", $._arg),
-    meta: ($) => prec.left(command("meta", seq($._txt_arg, $._arg))),
-    import: ($) => prec(2, command("import", braces(field("target", $.addr)))),
-    export: ($) => prec(2, command("export", braces(field("target", $.addr)))),
+    alloc: ($) => command_seq("alloc", $.identifier),
     namespace: ($) =>
-      command(
+      command_seq(
         "namespace",
         seq(
-          field("identifier", $.ident),
-          field("body", $._arg),
+          field("identifier", $.identifier),
+          field("body", $.argument),
         ),
       ),
-    transclude: ($) =>
-      choice(
-        prec(1, command("transclude", braces(field("target", $.addr)))),
-        command("transclude", $._arg),
-      ),
-    let: ($) => command("let", $.fun_spec),
-    datalog: ($) => command("datalog", $._opaque_arg),
-    diagram: ($) => command("diagram", $._opaque_arg),
-    tikzfig: ($) => command("tikzfig", $._opaque_arg),
-    tex: ($) => prec.right(command("tex", repeat1($._opaque_arg))),
-    scope: ($) => command("scope", $._arg),
+    let: ($) => command_seq("let", $.fun_spec),
+    datalog: ($) => command_seq("datalog", field("argument", $.opaque_argument)),
+    tex: ($) =>
+      prec.right(command_seq("tex", repeat1(field("argument", $.opaque_argument)))),
+    scope: ($) => command_seq("scope", $.argument),
     subtree: ($) =>
       prec.left(
-        command(
+        command_seq(
           "subtree",
-          seq(optional(squares(field("target", $.addr))), $._arg),
+          seq(optional(squares(field("target", $.addr))), $.argument),
         ),
       ),
     put: ($) =>
-      command(
+      command_seq(
         "put",
-        seq(field("identifier", $.ident), field("argument", $._arg)),
+        seq(field("identifier", $.identifier), field("argument", $.argument)),
       ),
     default: ($) =>
-      command(
+      command_seq(
         "put?",
-        seq(field("identifier", $.ident), field("argument", $._arg)),
+        seq(field("identifier", $.identifier), field("argument", $.argument)),
       ),
-    get: ($) => command("get", $.ident),
-    open: ($) => command("open", $.ident),
-    execute: ($) => command("execute", $._arg),
-    route_asset: ($) => command("route-asset", $._txt_arg),
-    current_tree: ($) => seq("\\", "current-tree"),
-    syndicate_query_as_json_blob: ($) =>
-      command(
-        "syndicate-query-as-json-blob",
-        seq(field("name", $._txt_arg), field("query", $._arg)),
-      ),
-    syndicate_current_tree_as_atom_feed: ($) =>
-      seq("\\", "syndicate-current-tree-as-atom-feed"),
-    query_tree: ($) => prec(2, command("query", $._arg)),
+    get: ($) => command_seq("get", $.identifier),
+    open: ($) => command_seq("open", $.identifier),
     object: ($) =>
       prec.left(
-        command(
+        command_seq(
           "object",
           seq(
             optional(field("self", squares($.text))),
@@ -195,38 +124,24 @@ module.exports = grammar({
 
     patch: ($) =>
       prec.left(
-        command(
+        command_seq(
           "patch",
           seq(
-            field("object", $._arg),
+            field("object", $.argument),
             optional($.patch_bindings),
             braces(repeat(choice($.method_decl, $._whitespace))),
           ),
         ),
       ),
     call: ($) =>
-      command(
+      command_seq(
         "call",
         seq(
-          field("object", $._arg),
+          field("object", $.argument),
           field("method", $._txt_arg),
         ),
       ),
     comment: ($) => /%[^\r\n]*/,
-    _prim: ($) =>
-      choice(
-        $.p,
-        $.em,
-        $.strong,
-        $.li,
-        $.ul,
-        $.ol,
-        $.code,
-        $.blockquote,
-        $.pre,
-        $.figure,
-        $.figcaption,
-      ),
     hash_ident: ($) => token(seq("#", /[A-Za-z0-9-]+/)),
     inline_math: ($) => seq("#", $._opaque_arg),
     display_math: ($) => seq("##", $._opaque_arg),
@@ -243,17 +158,6 @@ module.exports = grammar({
     linebreak: ($) => "\\\\",
     control_symbol: ($) => token(seq("\\", /[^A-Za-z0-9\\< \t\r\n]/)),
 
-    p: ($) => command("p", $._arg),
-    em: ($) => command("em", $._arg),
-    strong: ($) => command("strong", $._arg),
-    li: ($) => command("li", $._arg),
-    ul: ($) => command("ul", $._arg),
-    ol: ($) => command("ol", $._arg),
-    code: ($) => command("code", $._arg),
-    blockquote: ($) => command("blockquote", $._arg),
-    pre: ($) => command("pre", $._arg),
-    figure: ($) => command("figure", $._arg),
-    figcaption: ($) => command("figcaption", $._arg),
     patch_bindings: ($) =>
       choice(
         seq(
@@ -266,42 +170,72 @@ module.exports = grammar({
     method_decl: ($) =>
       seq(
         field("key", squares($.text)),
-        field("value", alias($._arg, $.method_body)),
+        field("value", alias($.argument, $.method_body)),
       ),
     fun_spec: ($) =>
       field(
         "function",
         seq(
-          field("identifier", $.ident),
+          field("identifier", $.identifier),
           choice(
-            field("argument", $._arg),
+            field("argument", $.argument),
             seq(
               field("binder", repeat1(squares($.text))),
-              field("argument", $._arg),
+              field("argument", $.argument),
             ),
           ),
         ),
       ),
-    ident: ($) =>
-      prec.left(seq("\\", $.text_identifier, optional(choice(repeat1($._arg), "{}")))),
-    _arg: ($) => braces(optional(repeat1(choice($._node)))),
+    command: ($) =>
+      choice(
+        prec.right(
+          2,
+          seq(
+            "\\",
+            field("name", $.command_name),
+            repeat1(field("argument", $.argument)),
+          ),
+        ),
+        seq("\\", field("name", $.command_name)),
+      ),
+    identifier: ($) => seq("\\", field("name", $.command_name)),
+    argument: ($) => braces(optional(repeat1(choice($._node)))),
     _opaque_arg: ($) =>
+      braces(repeat(choice(alias($._opaque_braces, $.braces), alias($._opaque_text, $.text)))),
+    opaque_argument: ($) =>
       braces(repeat(choice(alias($._opaque_braces, $.braces), alias($._opaque_text, $.text)))),
     _opaque_braces: ($) =>
       braces(repeat(choice(alias($._opaque_braces, $.braces), alias($._opaque_text, $.text)))),
     _opaque_text: ($) => /([^{}]|\\[{}])+/,
     _link: ($) => choice($.markdown_link, $.unlabeled_link),
     addr: ($) => prec(1, $.text),
-    id: ($) => repeat1(choice($._alpha, $._digit, "-", "_")),
-    prefix: ($) => repeat1($._alpha),
     markdown_link: ($) =>
-      seq(
+      prec(
+        1,
+        seq(
         "[",
-        field("label", $.text),
+        field(
+          "label",
+          alias(
+            repeat1(
+              choice(
+                $.text,
+                $.inline_math,
+                $.display_math,
+                $.command,
+                $.hash_ident,
+                $.linebreak,
+                $.control_symbol,
+              ),
+            ),
+            $.link_label,
+          ),
+        ),
         "]",
         token.immediate("("),
-        field("dest", choice($.addr, $.text)),
+        field("dest", $.addr),
         ")",
+        ),
       ),
     unlabeled_link: ($) =>
       seq("[[", field("target", choice($.addr, $.external_link)), "]]"),
@@ -310,7 +244,7 @@ module.exports = grammar({
     _alpha: ($) => /[a-zA-Z]+/,
     _digit: ($) => /[0-9]+/,
     text: ($) => /[^%#\\\{\}\[\]\(\)\r\n]+/,
-    text_identifier: ($) => /[^%#\\\{\}\[\]\(\)\ \r\n]+/,
+    command_name: ($) => /[^%#\\\{\}\[\]\(\)\ \r\n]+/,
     _txt_arg: ($) => braces($.text),
     _xml_base_ident: ($) =>
       seq($._alpha, repeat(choice($._alpha, $._digit, /[-/#]/))),
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -10,42 +10,13 @@
  "]"
 ] @punctuation.bracket
 
-(p "p" @function.builtin)
-(li "li" @markup.list)
-(ul "ul"  @markup.list)
-(ol "ol"  @markup.list)
-(em "em"  @function.builtin)
-(strong "strong" @function.builtin)
-(code "code" @function.builtin)
-(figure "figure" @function.builtin)
-(figcaption "figcaption" @function.builtin)
-
-(tag "tag" @field)
-(author "author" @field)
-(author_literal "author/literal" @field)
-(contributor "contributor" @field)
-(contributor_literal "contributor/literal" @field)
-(title "title" @field)
-(taxon "taxon" @field)
-(meta "meta" @field)
-(parent "parent" @field)
-(number "number" @field)
-
-(title "title" @text.title)
-(title (_) @text.title)
-(author (_) @markup.heading.url)
-(contributor (_) @markup.heading.url)
-
-(ident (text_identifier) @string)
+(command name: (command_name) @function.builtin)
+(identifier name: (command_name) @string)
 (hash_ident) @method
-(transclude "transclude" @include)
-(transclude target: (_) @markup.link.url)
-(ref target: (_) @markup.link.url)
-(import target: (_) @markup.link.url)
-(export target: (_) @markup.link.url)
 
 (def "def" @keyword)
 (fun "fun" @keyword)
+(let "let" @keyword)
 (object "object" @constant)
 (object self: (_) @keyword)
 (method_decl key: (_) @method)
@@ -62,25 +33,12 @@
 (default "put?" @variable.parameter)
 (open "open" @namespace)
 (namespace "namespace" @namespace)
-
-(query_tree "query" @keyword)
-;(query_author "query/author" @keyword)
-;(query_tag "query/tag" @keyword)
-;(query_taxon "query/taxon" @keyword)
-;(query_and "query/and" @keyword)
-;(query_or "query/or" @keyword)
-;(query_meta "query/meta" @keyword)
-
-(import "import" @include)
-(export "export" @include)
-(transclude "transclude" @include)
-(route_asset "route-asset" @include)
-(execute "execute" @keyword)
-(current_tree "current-tree" @variable.builtin)
+(get "get" @namespace)
+(alloc "alloc" @keyword)
+(subtree "subtree" @keyword)
+(datalog "datalog" @keyword)
+(tex "tex" @keyword)
 
 (inline_math) @string.special
 (display_math) @string.special
-(diagram) @string.special
-(tikzfig) @string.special
-(datalog) @string.special
 (verbatim_block_body) @string
